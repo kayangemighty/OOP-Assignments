8d281943af44987e27487a73452d8f36bfdbd0c5
kayangemighty <kayangemighty@gmail.com>
2026-04-23 08:09:32 -0700

    Implement Person class with swap functionality

--- C++ HW/Problem 1.cpp
+++ C++ HW/Problem 1.cpp
@@ -1,1 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+class Person {
+private:
+    char name[50];
+    int studentID;
+    int age;
+
+public:
+    void init(const char* inputName, int inputID, int inputAge) {
+        strcpy(name, inputName);
+        studentID = inputID;
+        age = inputAge;
+    }
+
+    void printStudentInfo() {
+        cout << "Name: " << name << ", ID: " << studentID << ", Age: " << age << endl;
+    }
+
+    int getID() {
+        return studentID;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    const char* getName() {
+        return name;
+    }
+};
+
+void personSwap(Person* a, Person* b) {
+    Person temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    Person p1, p2;
+    p1.init("Alice", 2026312345, 20);
+    p2.init("Bob", 2019354321, 25);
+
+    cout << "Before Swap:" << endl;
+    p1.printStudentInfo();
+    p2.printStudentInfo();
+
+    personSwap(&p1, &p2);
+
+    cout << endl;
+    cout << "After Swap:" << endl;
+    p1.printStudentInfo();
+    p2.printStudentInfo();
+
+    return 0;
+}
