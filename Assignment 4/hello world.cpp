6e0257e8700c4a060bf769d15c6f54a5a938522a
kayangemighty <kayangemighty@gmail.com>
2026-05-25 03:51:53 -0700

    Delete Assignment 4 directory

--- Assignment 4/hello world.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-void main(void) {
-    int num = 20;
-    std::cout << "Hello world!" << std::endl;
-    std::cout << "Hello " << "world!" << std::endl;
-    std::cout << num << ' ' << 'A';
-    std::cout << ' ' << 3.14 << std::endl;
-} // end of main
